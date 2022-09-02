4121f77798914ed0fd562c328ec36e5689d1204a
Anna <annagabriela2940@gmail.com>
2022-09-02 01:43:54 -0500

    feat(tailwind): tailwind integrated

---
+++ weather-app/tailwind.config.js
@@ -0,0 +1,31 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  mode: "jit",
+  content: {
+    files: [
+      "./components/**/*.{vue,js}",
+      "./layouts/**/*.vue",
+      "./pages/**/*.vue",
+      "./app.vue",
+      "./plugins/**/*.{js,ts}",
+      //"./nuxt.config.{js,ts}", // it is commented because https://github.com/nuxt-modules/tailwindcss/issues/429
+    ],
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
