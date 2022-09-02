85b161f2694f5cc135c3dc7aeb83f76a2fdf9e32
Anna <annagabriela2940@gmail.com>
2022-09-01 22:05:24 -0500

    feat(layouts): added a default layout with the navbar

---
+++ top-restaurants/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <Nav />
+        <!-- slot will be where the content defined in the page.vue
+        will be rendered -->
+        <slot />
+    </div>
+</template>
--- top-restaurants/pages/restaurants/index.vue
+++ top-restaurants/pages/restaurants/index.vue
@@ -1,7 +1,5 @@
 <template>
     <div>
-        <Nav />
-        <h1>Hello this is the restaurants</h1>
         <RestaurantTable/>
     </div>
 </template>
