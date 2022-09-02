567fec9872d786ebf2b17a1208cfe79f71c5bb98
Anna <annagabriela2940@gmail.com>
2022-09-01 22:01:11 -0500

    feat(pages): 404 page added

---
+++ top-restaurants/pages/404.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <Nav/>
+      <div class="container">
+        <h1>Page not found</h1>
+        <a href="/">Go Back</a>
+      </div> 
+      
+    </div>
+  </template>
+  
+  <style scoped>
+    .container {
+      text-align: center;
+      margin-top: 5rem;
+    }
+  </style>
+  
