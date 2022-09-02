2fc459782e93dcc19e4c578848757244e8e33f92
Anna <annagabriela2940@gmail.com>
2022-09-01 21:50:29 -0500

    feat(pages): dynamic paths added to restaurants

---
+++ top-restaurants/pages/restaurants/[name].vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        {{ name }}
+    </div>
+</template>
+
+<script setup lang="ts">
+
+// This is so the const name = route.params.name wont appear as an error
+// it is telling that 'name' is scoped to this page
+// (because there is a 'name' outside and it thinks we are re-writing it)
+import {} from "#app" 
+
+const route = useRoute()
+const name = route.params.name
+
+</script>
