5ee39dd174eae5627f0d9f01f6c0df6da456666f
Anna <annagabriela2940@gmail.com>
2022-09-01 21:53:46 -0500

    fix(pages): get data from the json in the dynamic paths

--- top-restaurants/pages/restaurants/[name].vue
+++ top-restaurants/pages/restaurants/[name].vue
@@ -1,17 +1,20 @@
 <template>
     <div>
-        {{ name }}
+        {{ restaurant }}
     </div>
 </template>
 
 <script setup lang="ts">
 
-// This is so the const name = route.params.name wont appear as an error
+/* // This is so the const name = route.params.name wont appear as an error
 // it is telling that 'name' is scoped to this page
 // (because there is a 'name' outside and it thinks we are re-writing it)
-import {} from "#app" 
+import {} from "#app" */
+import restaurants from '@/data.json'
 
 const route = useRoute()
 const name = route.params.name
+
+const restaurant = restaurants.find(r => r.name === name)
 
 </script>
