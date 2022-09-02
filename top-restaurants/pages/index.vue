ee896ec66b550bdb2cbad669f9c9f27eae0594ee
Anna <annagabriela2940@gmail.com>
2022-09-01 22:58:53 -0500

    fix(pages): added htmlmetadata to index

--- top-restaurants/pages/index.vue
+++ top-restaurants/pages/index.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
+      <Html
+      :lang="'eng-US'">
+        <Title>Restaurantly</Title>
+        <Meta 
+        name="description"
+        content="This is a description of the page"/>
+        <Link
+        rel="preload"
+        href="https://cdn-icons-png.flaticon.com/512/3305/3305803.png"
+        as="script" />
+      </Html>
       <div class="container">
         <h1>Welcome to Restaurantly</h1>
         <a href="/restaurants">Go to restaurants</a>
--- top-restaurants/pages/restaurants/index.vue
+++ top-restaurants/pages/restaurants/index.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
+        <Html
+        :lang="'eng-US'">
+            <Title>Top 50 restaurants</Title>
+            <Meta 
+            name="description"
+            content="This is a description of the page"/>
+            <Link
+            rel="preload"
+            href="https://cdn-icons-png.flaticon.com/512/3305/3305803.png"
+            as="script" />
+        </Html>
+
         <NuxtLayout name="restaurantad">
             <div class="container">
                 <RestaurantTable />
