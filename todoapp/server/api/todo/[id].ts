4777c51aa31ea2586d746413ee85fb49f9759991
Anna <annagabriela2940@gmail.com>
2022-09-10 20:41:00 -0500

    fix(server): id extracted from the path in api/todo/[id]

--- todoapp/server/api/todo/[id].ts
+++ todoapp/server/api/todo/[id].ts
@@ -1,5 +1,9 @@
 export default defineEventHandler((e) => {
     const method = e.req.method
+    const context = e.context
+    console.log(e.context.params)
 
-
+    if ( method === "PUT" ) {
+        const id = context.params.id
+    }
 })
