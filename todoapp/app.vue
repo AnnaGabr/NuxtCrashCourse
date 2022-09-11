e41715953f2b3befa357c52a2a8f037aff1bd598
Anna <annagabriela2940@gmail.com>
2022-09-11 11:48:04 -0500

    fix(app): autorefresh after functions

--- todoapp/app.vue
+++ todoapp/app.vue
@@ -52,21 +52,27 @@ input {
 
 <script setup lang="ts">
 
-const { data: todos } = useFetch("/api/todo")
+//const { data: todos } = useFetch("/api/todo")
+const { data: todos, refresh } = useAsyncData("todos", async () => {
+  return $fetch("/api/todo")
+})
 
 const input = ref("")
 
 const addTodo = async () => {
   if ( !input ) return;
   await $fetch("/api/todo", {method: "post", body: {item: input.value}})
+  refresh()
 }
 
 const updateTodo = async (id) => {
   await $fetch(`/api/todo/${id}`, {method: "put"})
+  refresh()
 }
 
 const deleteTodo = async (id) => {
   await $fetch(`/api/todo/${id}`, {method: "delete"})
+  refresh()
 }
 
 </script>
