8d73039f3038e989fa6a4bcdc26616285730d252
Anna <annagabriela2940@gmail.com>
2022-09-10 20:46:51 -0500

    fix(server): put method added to endpoint apo/todo/[id]

--- todoapp/server/api/todo/[id].ts
+++ todoapp/server/api/todo/[id].ts
@@ -1,3 +1,5 @@
+import { db } from "../../db"
+
 export default defineEventHandler((e) => {
     const method = e.req.method
     const context = e.context
@@ -5,5 +7,19 @@ export default defineEventHandler((e) => {
 
     if ( method === "PUT" ) {
         const id = context.params.id
+        let index
+        const todo = db.todos.find((todoItem, idx) => {
+            if ( todoItem.id === id ) {
+                index = idx
+                return true
+            } else { return false }
+        })
+        if (!todo) throw new Error()
+        const updatedTodo = {
+            ...todo,
+            completed: !todo.completed
+        }
+        db.todos[index] = updatedTodo
+        return updatedTodo
     }
 })
